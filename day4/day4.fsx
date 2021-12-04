d8b86c70db3f9efdc99e35d812e1680768a7644e
Patrick Johnstone <johnstonep@gmail.com>
2021-12-04 15:47:55 +0000

    Day4 part1 built boards

--- day4/day4.fsx
+++ day4/day4.fsx
@@ -22,13 +22,34 @@ type Board = {
 }
 
 let getRow (line: string) =
-  line.Split ','
+  line.Split ' '
   |> Array.toList
+  |> List.filter (fun v -> v.Length > 0)
   |> List.map (fun v -> { Value = (System.Int32.Parse(v)); Called = false })
 
-let rec getBoard lines board =
+let rec getBoardRec lines board =
   match lines with
   | [] -> board
   | _ ->
     let newRow = { Spaces = getRow lines.Head }
-    getBoard lines.Tail { board with Rows = board.Rows@[newRow] }
+    getBoardRec lines.Tail { board with Rows = board.Rows@[newRow] }
+
+let getBoard lines =
+  getBoardRec lines { Rows = [] }
+
+let rec getBoardsRec lines (boards: Board list) =
+  match lines with
+  | [] -> boards
+  | _ ->
+    match lines.Head with
+    | "" -> getBoardsRec lines.Tail boards
+    | _ ->
+      let newBoard = getBoard lines.[0..4]
+      let newBoards = newBoard::boards
+      let newLines = lines.[5..]
+      getBoardsRec newLines newBoards
+
+let getBoards lines =
+  getBoardsRec lines []
+
+getBoards example.Tail
