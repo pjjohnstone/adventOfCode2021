168cda2632ef99d13f4f5216a0c51742bdd752a1
Patrick Johnstone <patrick.johnstone@spektrix.com>
2021-12-08 16:14:20 +0000

    Day8 part1 solved

--- day8/day8.fsx
+++ day8/day8.fsx
@@ -1,11 +1,17 @@
 open System.IO
 
 let example = File.ReadAllLines("example.txt")
+let input = File.ReadAllLines("input.txt")
 
 type SignalPattern = {
   Inputs: string list;
   Outputs: string list;
 }
+
+let chopUpString (string: string) =
+  string.Split ' '
+  |> Array.toList
+  |> List.filter (fun s -> s.Length > 0)
 
 let getInputsAndOutputs (strings: string[]) =
   let patterns = strings |> Array.toList
@@ -15,8 +21,24 @@ let getInputsAndOutputs (strings: string[]) =
       pattern.Split '|'
       |> Array.toList
       |> fun l -> (l.Head, List.last l)
-      |> fun (i,o) -> (i.Split ' ' |> Array.toList, o.Split ' ' |> Array.toList)
+      |> fun (i,o) -> (chopUpString i, chopUpString o)
     yield { Inputs = inputs; Outputs = outputs }
-  }
+  } |> Seq.toList
 
-printfn "%A" (getInputsAndOutputs example)
+let knownOutput (output: string) =
+  match output.Length with
+  | 2 -> true
+  | 3 -> true
+  | 4 -> true
+  | 7 -> true
+  | _ -> false
+
+let countKnownOutputs (patterns: SignalPattern list) =
+  seq {
+    for pattern in patterns do
+    yield pattern.Outputs |> List.filter knownOutput
+  } |> List.concat |> List.length
+
+let signalPatterns = getInputsAndOutputs input
+
+printfn "There were: %i" (countKnownOutputs signalPatterns)
