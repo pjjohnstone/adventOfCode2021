52a05f58ecfcf720d98c31b987eec98c4ab16737
Patrick Johnstone <johnstonep@gmail.com>
2021-12-08 22:43:58 +0000

    Part2 WIP decode this mess

--- day8/day8.fsx
+++ day8/day8.fsx
@@ -8,6 +8,23 @@ type SignalPattern = {
   Outputs: string list;
 }
 
+type DisplayMapping = {
+  Top: char[]
+  UpperLeft: char[]
+  LowerLeft: char[]
+  Middle: char[]
+  Bottom: char[]
+  UpperRight: char[]
+  LowerRight: char[]
+}
+
+type Numbers = {
+  One: char[]
+  Four: char[]
+  Seven: char[]
+  Eight: char[]
+}
+
 let chopUpString (string: string) =
   string.Split ' '
   |> Array.toList
@@ -33,12 +50,71 @@ let knownOutput (output: string) =
   | 7 -> true
   | _ -> false
 
+let getKnownNumbers (inputs: string list) =
+  let one = inputs |> List.find (fun s -> s.Length = 2) |> Seq.toArray
+  let seven = inputs |> List.find (fun s -> s.Length = 3) |> Seq.toArray
+  let four = inputs |> List.find (fun s -> s.Length = 4) |> Seq.toArray
+  let eight = inputs |> List.find (fun s -> s.Length = 7) |> Seq.toArray
+  { One = one; Four = four; Seven = seven; Eight = eight}
+
+let rec containsAllElements list2 list1 =
+  match list1 with
+  | [] -> false
+  | _ ->
+    match list2 with
+    | [] -> true
+    | h::t ->
+      if not (List.contains h list1) then false
+      else containsAllElements t list1
+
+let searchStringListForChars chars stringList =
+  stringList
+  |> List.find (fun s ->
+    s
+    |> Seq.toList
+    |> containsAllElements (Array.toList chars))
+  |> Seq.toArray
+
+let searchStringforChars chars string =
+  string |> Seq.toList |> containsAllElements (Array.toList chars)
+
+let resolveMappings one seven four eight (inputs: string list) =
+  let top = seven |> Array.except one
+  let upperLeftAndMiddle = four |> Array.except one
+  let lowerLeftAndBottom = eight |> Array.except (Array.concat [four;top])
+  let zero = inputs |> searchStringListForChars (Array.append seven lowerLeftAndBottom)
+  let middle = upperLeftAndMiddle |> Array.except zero
+  let upperLeft = upperLeftAndMiddle |> Array.except middle
+  let three = inputs |> searchStringListForChars (Array.append seven middle)
+  let lowerLeft = lowerLeftAndBottom |> Array.except three
+  let bottom = lowerLeftAndBottom |> Array.except lowerLeft
+  let six = inputs |> List.find (fun i -> (i.Length = 6) && (searchStringforChars lowerLeft i))
+  let upperRight = one |> Array.except six
+  let lowerRight = one |> Array.except upperRight
+  {
+    Top = top;
+    UpperLeft = upperLeft;
+    LowerLeft = lowerLeft;
+    Middle = middle;
+    Bottom = bottom;
+    UpperRight = upperRight;
+    LowerRight = lowerRight;
+  }
+
 let countKnownOutputs (patterns: SignalPattern list) =
   seq {
     for pattern in patterns do
     yield pattern.Outputs |> List.filter knownOutput
   } |> List.concat |> List.length
 
-let signalPatterns = getInputsAndOutputs input
+let signalPatterns = getInputsAndOutputs example
 
-printfn "There were: %i" (countKnownOutputs signalPatterns)
+printfn "There were %i known outputs" (countKnownOutputs signalPatterns)
+
+let shortExample = [|"acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"|]
+
+let io = getInputsAndOutputs shortExample |> List.exactlyOne
+
+let knownDigits = getKnownNumbers io.Inputs
+
+let mapped = resolveMappings knownDigits.One knownDigits.Seven knownDigits.Four knownDigits.Eight io.Inputs
