21db626cd15da7ddcdeec2d806fbdc02bd126c0e
Patrick Johnstone <patrick.johnstone@spektrix.com>
2021-12-26 11:35:18 +0000

    Day13 some list length problems

--- day13/day13.fsx
+++ day13/day13.fsx
@@ -1,6 +1,7 @@
 open System.IO
 
 let example = File.ReadAllLines("example.txt")
+let input = File.ReadAllLines("input.txt")
 
 type Coordinate = {
   X: int;
@@ -34,6 +35,36 @@ let getFolds lines =
   lines
   |> Array.skip (index + 1)
   |> Array.map parseFoldInstruction
+  |> Array.toList
 
-let coords = getCoords example
-let folds = getFolds example
+let foldY foldPosition point =
+  let distance = point.Y - foldPosition
+  if point.Y >= foldPosition then { point with Y = (point.Y - distance * 2) } else point
+
+let foldX foldPosition point =
+  let distance = point.X - foldPosition
+  if point.X >= foldPosition then { point with X = (point.X - distance * 2) } else point
+
+let fold coords fold =
+  match fold.Axis with
+  | 'y' -> List.map (foldY fold.Position) coords
+  | _ -> List.map (foldX fold.Position) coords
+
+let drawPattern coords =
+  let lengthY = coords |> List.maxBy (fun c -> c.Y) |> fun c -> c.Y
+  let lengthX = coords |> List.maxBy (fun c -> c.X) |> fun c -> c.X
+  let grid = Array2D.create (lengthY + 1) (lengthX + 1) '.'
+  coords
+  |> List.iter (fun c -> grid.[c.Y,c.X] <- 'O')
+  printfn "%A" grid
+
+let countHoles coords =
+  coords |> List.distinct |> List.length |> printfn "There are %i holes"
+
+let coords = getCoords input
+let folds = getFolds input
+// drawPattern coords
+countHoles coords
+let newCoords = fold coords folds.Head
+// drawPattern newCoords
+countHoles newCoords
